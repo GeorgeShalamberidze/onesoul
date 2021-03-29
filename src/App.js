dba1b73188cd50635993cc2dfd67ce5c4c83f9bc
George Shalamberidze <giorgishalamberidze1995@gmail.com>
2021-03-29 12:42:02 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import {
 } from "react-router-dom";
 import Feed from './components/Feed'
 import Interest from './components/Interest'
-import LandingPage from "components/LandingPage";
-import Goals from "/components/Goals"
+import LandingPage from "./components/LandingPage";
+import Goals from "./components/Goals"
 import ServiceSinglePage from "./components/ServiceSinglePage"
 import Account from "./components/Account"
 
@@ -16,9 +16,9 @@ function App() {
     <Router>
       <div>
         <Switch>
-          <Route exact path="https://georgeshalamberidze.github.io/onesoul/" component={LandingPage} />
+          <Route exact path="/" component={LandingPage} />
           <Route exact path="/interest" component={Interest} />
-          <Route exact path="https://georgeshalamberidze.github.io/onesoul/goals" component={Goals} />
+          <Route exact path="/goals" component={Goals} />
           <Route exact path="/feed" component={Feed} />
           <Route exact path="/service" component={ServiceSinglePage} />
           <Route exact path="/account" component={Account} />
